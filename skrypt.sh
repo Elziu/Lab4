a74f4d68949f0b43c12a14267c3117f418926ae2
Elziu <jan.jakub.mielziuk@gmail.com>
2024-06-29 13:38:56 +0200

    Add help option to the script

--- skrypt.sh
+++ skrypt.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
-  date
-fi
-#!/bin/bash
+# Function to display the current date
+display_date() {
+    date
+}
 
-if [ "$1" == "--logs" ]; then
-  for i in {1..100}; do
-    echo -e "log$i.txt
-$0
-$(date)" > log$i.txt
-  done
-fi
-#!/bin/bash
+# Function to create log files
+create_logs() {
+    num_files=${1:-100}
+    for i in $(seq 1 $num_files); do
+        echo -e "log$i.txt\n$0\n$(date)" > log$i.txt
+    done
+}
 
-if [ "$1" == "--logs" ]; then
-  num_files=${2:-100}
-  for i in $(seq 1 $num_files); do
-    echo -e "log$i.txt
-$0
-$(date)" > log$i.txt
-  done
-fi
+# Function to display help information
+display_help() {
+    echo "Available options:"
+    echo "--date: Display today's date"
+    echo "--logs [num]: Generate log files"
+    echo "--help: Display help information"
+}
+
+# Main script logic
+case "$1" in
+    --date)
+        display_date
+        ;;
+    --logs)
+        create_logs "$2"
+        ;;
+    --help)
+        display_help
+        ;;
+    *)
+        echo "Invalid option. Use --help to see the available options."
+        ;;
+esac
