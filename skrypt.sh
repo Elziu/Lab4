7c01175e8159828197ed5cbd61e97c13e3f19682
Elziu <jan.jakub.mielziuk@gmail.com>
2024-06-29 13:28:45 +0200

    Add feature to generate a specified number of log files

--- skrypt.sh
+++ skrypt.sh
@@ -12,3 +12,13 @@ $0
 $(date)" > log$i.txt
   done
 fi
+#!/bin/bash
+
+if [ "$1" == "--logs" ]; then
+  num_files=${2:-100}
+  for i in $(seq 1 $num_files); do
+    echo -e "log$i.txt
+$0
+$(date)" > log$i.txt
+  done
+fi
